150128e9cdf85b53116529ae0e915e1c5c59d7c7
hrshkshri <harshkeshari100@gmail.com>
2023-12-02 00:04:09 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </h1>
+  )
+}
+
+export default App
