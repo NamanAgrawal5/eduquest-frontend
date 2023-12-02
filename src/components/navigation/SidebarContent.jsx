ba687758927218129d4d029224cd6835dbd4fc9e
hrshkshri <harshkeshari100@gmail.com>
2023-12-02 23:15:03 +0530

    added layout

---
+++ src/components/navigation/SidebarContent.jsx
@@ -0,0 +1,30 @@
+import { Box, CloseButton, Flex, Text, useColorModeValue } from '@chakra-ui/react';
+import LinkItems from './Links';
+import NavItem from './NavItem';
+
+const SidebarContent = ({ onClose, ...rest }) => {
+    return (
+      <Box
+        transition="3s ease"
+        bg={useColorModeValue('white', 'gray.900')}
+        w={{ base: 'full', md: 60 }}
+        pos="fixed"
+        h="full"
+        {...rest}
+      >
+        <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+          <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+            Logo
+          </Text>
+          <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+        </Flex>
+        {LinkItems.map((link) => (
+          <NavItem key={link.name} icon={link.icon}>
+            {link.name}
+          </NavItem>
+        ))}
+      </Box>
+    );
+  };
+
+export default SidebarContent
